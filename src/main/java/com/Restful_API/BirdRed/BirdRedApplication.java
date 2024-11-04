bf0dd2883bafd536bd22b9bd81adb8e966baf2a2
phenriqued <pedrodias3737@gmail.com>
2024-11-04 17:11:45 -0300

    Piloto

---
+++ src/main/java/com/Restful_API/BirdRed/BirdRedApplication.java
@@ -0,0 +1,13 @@
+package com.Restful_API.BirdRed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdRedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdRedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Restful_API/BirdRed/BirdRedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Restful_API.BirdRed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdRedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
